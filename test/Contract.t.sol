b309f8eba4c9bcb20b098beb007720009ab8d76c
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-04-30 21:43:36 +0200

    prank & other cheatcodes + fuzz testing

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -6,7 +6,18 @@ import "forge-std/Vm.sol";
 import "solmate/test/utils/mocks/MockERC20.sol";
 import "../src/Contract.sol";
 
-contract ContractTest is DSTest {
+contract TokenReturner {
+    uint256 return_amount;
+
+    function receiveTokens(
+        address tokenAddress,
+        uint256 /* amount */
+    ) external {
+        ERC20(tokenAddress).transfer(msg.sender, return_amount);
+    }
+}
+
+contract ContractTest is DSTest, TokenReturner {
     Vm vm = Vm(HEVM_ADDRESS);
 
     address alice = address(0x1337);
@@ -45,6 +56,62 @@ contract ContractTest is DSTest {
         token.approve(address(loaner), 1);
         loaner.depositTokens(1);
         assertEq(loaner.poolBalance(), 101);
+        assertEq(token.balanceOf(address(loaner)), loaner.poolBalance());
+    }
+
+    function test_BorrowZeroRevert() public {
+        vm.expectRevert(Flashloaner.MustBorrowOneTokenMinimum.selector);
+        loaner.flashLoan(0);
+    }
+
+    function test_NotEnoughTokensInPoolRevert() public {
+        vm.expectRevert(Flashloaner.NotEnoughTokensInPool.selector);
+        loaner.flashLoan(101);
+    }
+
+    function test_ReturnAmountRevert() public {
+        vm.expectRevert(Flashloaner.FlashLoanHasNotBeenPaidBack.selector);
+        return_amount = 0;
+        loaner.flashLoan(100);
+    }
+
+    function test_flashloan() public {
+        return_amount = 100;
+        loaner.flashLoan(100);
+        assertEq(loaner.poolBalance(), 100);
+        assertEq(token.balanceOf(address(loaner)), loaner.poolBalance());
+    }
+
+    function test_onlyOwnerRevert() public {
+        vm.startPrank(bob);
+        vm.expectRevert("not owner");
+        loaner.updateOwner(bob);
+        loaner.echoSender();
+        vm.stopPrank();
+    }
+
+    function testFuzz_deposit(uint256 amount) public {
+        vm.assume(type(uint256).max - amount >= token.totalSupply());
+        vm.assume(amount > 0);
+        token.mint(address(this), amount);
+        token.approve(address(loaner), amount);
+
+        uint256 prebal = token.balanceOf(address(loaner));
+        loaner.depositTokens(amount);
+        assertEq(loaner.poolBalance(), prebal + amount);
+        assertEq(token.balanceOf(address(loaner)), loaner.poolBalance());
+    }
+
+    function testFuzz_FlashLoan(
+        uint256 borrow_amount,
+        uint256 _return_amount
+    ) public {
+        vm.assume(borrow_amount > 0);
+        vm.assume(_return_amount <= token.balanceOf(address(this)));
+        vm.assume(borrow_amount <= _return_amount);
+        vm.assume(borrow_amount <= token.balanceOf(address(loaner)));
+        return_amount = _return_amount;
+        loaner.flashLoan(borrow_amount);
         assertEq(token.balanceOf(address(loaner)), loaner.poolBalance());
     }
 }
